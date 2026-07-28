11795ebbf2d98f5946d895468e7cf53f18530247
kumar yedida <kumaryedida339@gmail.com>
2026-07-28 10:37:29 +0530

    backend

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)&d)k1myl$ujpim47&8#udrj7lp7oe^94ho+eh&%1)!acl(*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
