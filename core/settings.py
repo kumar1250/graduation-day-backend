5678fb95cfc8d331ef3d9b89381a28e5333c7e31
kumar yedida <kumaryedida339@gmail.com>
2026-08-07 06:23:18 +0530

    backend

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
